2ba180efbd99a498234f0814d20a0fc2998d58d3
Sergio Scotta <sergeen@gmail.com>
2025-06-27 17:00:06 -0300

    Add some general layout structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import styles from './App.module.css';
+
 function App() {
   return (
-    <div>
-        Hola!
+    <div class={styles.app}>
+      <div class={styles.modal}></div>
+      <div class={styles.topBar}>
+        <div class={styles.menu}></div>
+        <div class={styles.search}></div>
+        <div class={styles.brand}></div>
+        <div class={styles.user}></div>
+        <div class={styles.cart}></div>
+      </div>
+      <div class={styles.navBar}>
+        <div class={styles.navBarItem}></div>
+        <div class={styles.navBarItem}></div>
+        <div class={styles.navBarItem}></div>
+        <div class={styles.navBarItem}></div>
+        <div class={styles.navBarItem}></div>
+      </div>
+      <main>
+        <div class={styles.panel}></div>
+      </main>
     </div>
   );
 }
